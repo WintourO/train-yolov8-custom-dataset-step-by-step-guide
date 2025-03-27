a0d9166e91fc48705cab1834177d7e7a8334f311
WintourO <oscar.wintour@gmail.com>
2025-03-27 19:04:42 +1100

    File Locating Issues/Simplification

--- local_env/train.py
+++ local_env/train.py
@@ -4,4 +4,6 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 
 # Use the model
-results = model.train(data="config.yaml", epochs=1)  # train the model
+results = model.train(data=r"C:\Users\User\Documents\GitHub\train-yolov8-custom-dataset-step-by-step-guide\local_env\config.yaml", epochs=1)
+
+  # train the model
