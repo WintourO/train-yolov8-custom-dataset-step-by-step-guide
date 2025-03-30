8b7d5057e263a95ff39096140aae411d373a2a42
WintourO <oscar.wintour@gmail.com>
2025-03-30 19:18:51 +1100

    Surfers Data Added

---
+++ LabellingETC/ClassAllocator1.py
@@ -0,0 +1,30 @@
+import os
+
+label_dir = r"C:\Users\User\Desktop\Surfers\test\labels"
+
+# Define class mappings (old -> new)
+class_mapping = {5: 4}  # Change '2' (old person/surfer class) to '1'
+
+for filename in os.listdir(label_dir):
+    if filename.endswith(".txt"):
+        file_path = os.path.join(label_dir, filename)
+
+        with open(file_path, "r") as file:
+            lines = file.readlines()
+
+        new_lines = []
+        for line in lines:
+            parts = line.split()
+            class_id = int(parts[0])
+
+            # Update class ID if in mapping
+            if class_id in class_mapping:
+                parts[0] = str(class_mapping[class_id])
+
+            new_lines.append(" ".join(parts) + "\n")
+
+        # Overwrite with updated labels
+        with open(file_path, "w") as file:
+            file.writelines(new_lines)
+
+print("Label realignment complete!")
--- local_env/train.py
+++ local_env/train.py
@@ -8,16 +8,17 @@ print(torchvision.__version__)
 print(ultralytics.__version__)
 
 # Load a model
- #model = YOLO("yolov8n.yaml")  # build a new model from scratch
- #model.to('cuda')
+#model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("C:\\Users\\User\\Documents\\GitHub\\train-yolov8-custom-dataset-step-by-step-guide\\runs\\detect\\train7\\weights\\best.pt")
+model.to('cuda')
 
 
 # Use the model
-#results = model.train(data=r"C:\Users\User\Documents\GitHub\train-yolov8-custom-dataset-step-by-step-guide\local_env\config.yaml", epochs=200, workers=0)
+results = model.train(data=r"C:\Users\User\Documents\GitHub\train-yolov8-custom-dataset-step-by-step-guide\local_env\config.yaml", epochs=20, workers=0)
 
 # train the model
 
-model = YOLO("C:\\Users\\User\\Documents\\GitHub\\train-yolov8-custom-dataset-step-by-step-guide\\runs\\detect\\train7\\weights\\best.pt")
-results = model("C:\\Users\\User\\Desktop\\SharkTest2.jpg")
+#model = YOLO("C:\\Users\\User\\Documents\\GitHub\\train-yolov8-custom-dataset-step-by-step-guide\\runs\\detect\\train7\\weights\\best.pt")
+#results = model("C:\\Users\\User\\Desktop\\SharkTest2.jpg")
 
-print(results)
+#print(results)
